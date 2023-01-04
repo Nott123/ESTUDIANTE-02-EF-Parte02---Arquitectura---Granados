55ac930c64a63398ccec7b3d59672771698faa92
Adrian Granados <lgk9hd.10@gmail.com>
2023-01-04 12:00:39 -0500

    EF Parte02 - Producto -Granados

--- BGH_EF/miapp/models.py
+++ BGH_EF/miapp/models.py
@@ -1,3 +1,11 @@
+from django.db import models
+class Docente (models.Model):
+	codigo = models.TextField()
+	nombre = models.TextField()
+	dni = models.TextField()
+	Fecha_nacimiento = models.TextField()
+	Fecha_registro = models.DateTimeField(auto_now_add=True)
+	estado =  models.TextField(max_length=1)
 
 
 # Create your models here.
